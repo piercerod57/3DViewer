598e4fc9de690bf32c4cdcb4587b973e759e450a
dookadoo <35851914+dookadoo@users.noreply.github.com>
2021-05-05 14:36:28 -0600

    redo

---
+++ ModelViewer/ModelViewer/f/LoadModel.h
@@ -0,0 +1,44 @@
+#pragma once
+/**********************************************************
+ *
+ * FUNCTION Load3DS (obj_type_ptr, char *)
+ *
+ * This function loads a mesh from a 3ds file.
+ * Please note that we are loading only the vertices, polygons and mapping lists.
+ * If you need to load meshes with advanced features as for example:
+ * multi objects, materials, lights and so on, you must insert other chunk parsers.
+ *
+ *********************************************************/
+#define MAX_VERTICES 8000 // Max number of vertices (for each object)
+#define MAX_POLYGONS 8000 // Max number of polygons (for each object)
+
+// Our vertex type
+typedef struct {
+	float x, y, z;
+}vertex_type;
+
+// The polygon (triangle), 3 numbers that aim 3 vertices
+typedef struct {
+	int a, b, c;
+}polygon_type;
+
+// The mapcoord type, 2 texture coordinates for each vertex
+typedef struct {
+	float u, v;
+}mapcoord_type;
+
+// The object type
+typedef struct {
+	char name[20];
+
+	int vertices_qty;
+	int polygons_qty;
+
+	vertex_type vertex[MAX_VERTICES];
+	polygon_type polygon[MAX_POLYGONS];
+	mapcoord_type mapcoord[MAX_VERTICES];
+	int id_texture;
+} obj_type, *obj_type_ptr;
+
+
+extern char Load3DS(obj_type_ptr ogg, char *filename);
---
+++ ModelViewer/ModelViewer/f/ModelViewer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_ModelViewer.h"
+
+class ModelViewer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ModelViewer(QWidget *parent = Q_NULLPTR);
+
+
+	
+
+private:
+    Ui::ModelViewerClass ui;
+};
